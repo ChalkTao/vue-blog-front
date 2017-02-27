d442b37f28c2e6c8db70993d0d66aabf4bb58fff
Chalk <caotao2003@163.com>
2017-02-27 10:58:32 +0800

    add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div style="min-height: 500px;">
     <div class="wrapper-content">
       <h1>Hello, World!</h1>
+      <h2 v-for="item in userList"><a v-link="{name:'user', params: {user: item.nickname}}">{{item.nickname}}</a></h2>
     </div>
   </div>
 </template>
@@ -10,7 +11,18 @@
 export default {
   components: {
   },
-  data: function () {
+  data () {
+    return {
+      userList: []
+    }
+  },
+  created () {
+    this.$http.get('user/list').then(response => {
+      this.userList = response.data.data
+    }, response => {
+      var msg = (response.data && response.data.error_msg) || '获取失败'
+      this.$Message.error(msg)
+    })
   }
 }
 </script>
